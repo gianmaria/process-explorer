77b2e877a1b7422fefce92db5807dbd948aaeea6
Gianmaria Bonici <gianmaria.bonici@schindler.com>
2025-04-07 10:21:43 +0200

    some formatting

--- main.cpp
+++ main.cpp
@@ -3,90 +3,101 @@
 #include <tlhelp32.h>
 #include <psapi.h>
 
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include <format>
+#include <algorithm>
 
-
-using std::cout;
 using std::cerr;
+using std::cout;
 using std::endl;
+using std::format;
 using std::string;
 using std::vector;
-using std::format;
 
-int main()
+vector<DWORD> enum_all_pids()
 {
     constexpr DWORD PIDS_COUNT = 1024;
-    constexpr DWORD PIDS_SIZE = PIDS_COUNT * sizeof(DWORD);
-    DWORD pids[PIDS_COUNT] {};
+    auto pids = vector<DWORD>(PIDS_COUNT, 0);
 
     DWORD bytes_needed = 0;
+    constexpr DWORD PIDS_SIZE = PIDS_COUNT * sizeof(DWORD);
 
     BOOL res = EnumProcesses(
-        pids, // [out] DWORD   *lpidProcess,
-        PIDS_SIZE, // [in]  DWORD   cb,
+        pids.data(),  // [out] DWORD   *lpidProcess,
+        PIDS_SIZE,    // [in]  DWORD   cb,
         &bytes_needed // [out] LPDWORD lpcbNeeded
     );
-
-    auto pids_enumerated = bytes_needed / sizeof(DWORD);
 
     if (res == 0)
     {
         cerr << format("ERROR: EnumProcesses failed ({})", GetLastError()) << endl;
-        return 1;
+        return {};
     }
 
+    auto pids_enumerated = bytes_needed / sizeof(DWORD);
     if (pids_enumerated == PIDS_COUNT)
     {
         cerr << format("ERROR: Too much processes for array capacity") << endl;
-        return -1;
+        return {};
     }
 
-    for (DWORD i = 0;
-         i < pids_enumerated;
-         ++i)
+    return pids;
+}
+
+string collect_process_info(DWORD pid)
+{
+    HANDLE handle = OpenProcess(
+        PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, // [in] DWORD dwDesiredAccess,
+        FALSE,                                       // [in] BOOL  bInheritHandle,
+        pid                                          // [in] DWORD dwProcessId
+    );
+
+    if (handle == NULL) return {};
+
+    DWORD buffer_size = 1024;
+    auto buffer = string(buffer_size, 0);
+
+    DWORD size_needed = buffer_size;
+    BOOL res = QueryFullProcessImageNameA(
+        handle,        // [in]      HANDLE hProcess,
+        0,             // [in]      DWORD  dwFlags,
+        buffer.data(), // [out]     LPWSTR lpExeName,
+        &size_needed   // [in, out] PDWORD lpdwSize
+    );
+
+    if (res == 0 or
+        size_needed >= buffer_size)
     {
-        auto& pid = pids[i];
-        if (pid == 0 or pid == 4) continue;
-
-        HANDLE handle = OpenProcess(
-            PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, // [in] DWORD dwDesiredAccess,
-            FALSE, // [in] BOOL  bInheritHandle,
-            pid    // [in] DWORD dwProcessId
-        );
-
-        if (handle == NULL)
-        {
-            cout << format("pid: {} -", pid) << endl;
-            continue;
-        }
-
-        constexpr DWORD buffer_count = 1024;
-        char buffer[buffer_count] = {};
-
-        DWORD size = buffer_count;
-        res = QueryFullProcessImageNameA(
-            handle, // [in]      HANDLE hProcess,
-            0,      // [in]      DWORD  dwFlags,
-            buffer, // [out]     LPWSTR lpExeName,
-            &size   // [in, out] PDWORD lpdwSize
-        );
-
-        if (res == 0)
-        {
-            cerr << format("ERROR: QueryFullProcessImageNameW failes ({})", GetLastError()) << endl;
-            return 1;
-        }
-
-        cout << format("pid: {} - {}", pid, buffer) << endl;
-
+        cerr << format("ERROR: QueryFullProcessImageNameW failes ({})", GetLastError()) << endl;
         CloseHandle(handle);
+        return {};
     }
 
+    CloseHandle(handle);
+    return buffer;
+}
 
+int main()
+{
+    auto pids = enum_all_pids();
+
+    if (pids.size() == 0)
+        return -1;
+
+    // std::sort(pids.begin(), pids.end());
+
+    for (auto &pid : pids)
+    {
+        if (pid == 0 or pid == 4)
+            continue;
+
+        auto info = collect_process_info(pid);
+
+        cout << format("pid: {:<5} - {}", pid, info) << endl;
+
+    }
 
     return 0;
 }
